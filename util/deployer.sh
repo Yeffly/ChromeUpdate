e7f66d38ba6da150da968c58a20a0335a26fb2c1
翠花 <admin@nange.cn>
2020-12-18 19:47:28 +0800

    Update deployer.sh

--- util/deployer.sh
+++ util/deployer.sh
@@ -9,8 +9,9 @@ cd ./_deploy
 git init
 git config --global push.default matching
 git config --global user.email admin@nange.cn
-git config --global user.name XOS
+git config --global user.name 佩佩
 git remote add origin https://github.com/XOS/CheckChrome.git
+git remote add origin https://${{ secrets.USERNAME }}:${{ secrets.PASSWORD }} @github.com/XOS/CheckChrome.git
 git pull origin gh-pages
 rm -rf ./*
 cp -rf ../public/* ../_deploy/
