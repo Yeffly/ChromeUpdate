cabda0ca14fc6591fb053e050c5757df61854bf1
翠花 <admin@nange.cn>
2020-12-18 20:04:30 +0800

    Update deployer.sh

--- util/deployer.sh
+++ util/deployer.sh
@@ -12,6 +12,7 @@ git config --global user.email admin@nange.cn
 git config --global user.name 佩佩
 #git remote add origin https://github.com/XOS/CheckChrome.git
 git remote add origin https://${{ secrets.USERNAME }}:${{ secrets.PASSWORD }} @github.com/XOS/CheckChrome.git
+git remote -v
 git pull origin gh-pages
 rm -rf ./*
 cp -rf ../public/* ../_deploy/
@@ -20,5 +21,5 @@ touch CNAME
 echo 'chr.nan.ge' > ./CNAME
 
 git add --all .
-git commit -m "Daily check of Chrome offline packages by Travis CI"
+git commit -m "Daily check of Chrome offline packages"
 git push --quiet --force origin HEAD:gh-pages
