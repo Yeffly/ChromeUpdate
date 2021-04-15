e4c630ec983600ea38563b6c06a59e1cced6cd6f
佩佩 <admin@nange.cn>
2021-04-15 19:45:39 +0800

    Fix.

--- util/generator.sh
+++ util/generator.sh
@@ -3,31 +3,30 @@ echo '-------------------------------------'
 echo 'Start Generator ......'
 echo '-------------------------------------'
 
-for i in $@
-do
-    cache="http://101.96.9.133/"
+for i in $@; do
     version=$(sed -n '1p' ./tmp/parse/${i}-result.info)
     dl_base=$(sed -n '2p' ./tmp/parse/${i}-result.info)
     dl_alt_base=$(sed -n '3p' ./tmp/parse/${i}-result.info)
-    dl_file=$(sed -n '4p' ./tmp/parse/${i}-result.info)
+    dl_slave_base=$(sed -n '4p' ./tmp/parse/${i}-result.info)
+    dl_file=$(sed -n '6p' ./tmp/parse/${i}-result.info)
     dl="https://"${dl_base}${dl_file}
     dl_alt="https://"${dl_alt_base}${dl_file}
-    dl_cache=${cache}${dl_alt_base}${dl_file}
-    #hash=$(sed -n '5p' ./tmp/parse/${i}-result.info)
-    size_raw=$(sed -n '6p' ./tmp/parse/${i}-result.info)
+    dl_slave="https://"${dl_slave_base}${dl_file}
+    sha256=$(sed -n '5p' ./tmp/parse/${i}-result.info)
+    size_raw=$(sed -n '7p' ./tmp/parse/${i}-result.info)
     size=$(awk 'BEGIN{printf "%.3f",('$size_raw'/'1048576')}')" MB"
 
     sed -i "s|{{${i}-Version}}|$version|g" tmp/index.html
-    #sed -i "s|{{${i}-HASH}}|$hash|g" tmp/index.html
+    sed -i "s|{{${i}-HASH}}|$sha256|g" tmp/index.html
     sed -i "s|{{${i}-dl-main}}|$dl|g" tmp/index.html
     sed -i "s|{{${i}-dl-alternative}}|$dl_alt|g" tmp/index.html
-    sed -i "s|{{${i}-dl-cache}}|$dl_cache|g" tmp/index.html
+    sed -i "s|{{${i}-dl-slave}}|$dl_slave|g" tmp/index.html
     sed -i "s|{{${i}-Size}}|$size|g" tmp/index.html
 
     sed -i "s|{{${i}-Version}}|$version|g" tmp/chrome.xml
-    #sed -i "s|{{${i}-HASH}}|$hash|g" tmp/chrome.xml
+    sed -i "s|{{${i}-HASH}}|$sha256|g" tmp/chrome.xml
     sed -i "s|{{${i}-dl-main}}|$dl|g" tmp/chrome.xml
     sed -i "s|{{${i}-dl-alternative}}|$dl_alt|g" tmp/chrome.xml
-    sed -i "s|{{${i}-dl-cache}}|$dl_cache|g" tmp/chrome.xml
+    sed -i "s|{{${i}-dl-slave}}|$dl_slave|g" tmp/chrome.xml
     sed -i "s|{{${i}-Size}}|$size_raw|g" tmp/chrome.xml
 done
