a6847d6166b7f9d816766c541de457917adc3b46
佩佩 <admin@nange.cn>
2021-08-20 02:59:11 +0800

    Update.

--- util/parse-mac.sh
+++ util/parse-mac.sh
@@ -6,11 +6,11 @@ echo '-------------------------------------'
 for i in $@; do
     echo 'Parsing mac '${i}' data ......'
     
-    sed -n '14p' ./tmp/parse/stable-x64.info > ./tmp/parse/stable-inter-result.info
-    sed -n '21p' ./tmp/parse/stable-x64.info >> ./tmp/parse/stable-inter-result.info
-    sed -n '23p' ./tmp/parse/stable-x64.info >> ./tmp/parse/stable-inter-result.info
-    echo 'dl.google.com/chrome/mac/stable/GGRO/' >> ./tmp/parse/stable-inter-result.info
-    echo 'googlechrome.dmg' >> ./tmp/parse/stable-inter-result.info
+    sed -n '14p' ./tmp/parse/stable-x64.info > ./tmp/parse/stable-intel-result.info
+    sed -n '21p' ./tmp/parse/stable-x64.info >> ./tmp/parse/stable-intel-result.info
+    sed -n '23p' ./tmp/parse/stable-x64.info >> ./tmp/parse/stable-intel-result.info
+    echo 'dl.google.com/chrome/mac/stable/GGRO/' >> ./tmp/parse/stable-intel-result.info
+    echo 'googlechrome.dmg' >> ./tmp/parse/stable-intel-result.info
     
     sed -n '14p' ./tmp/parse/stable-x86.info > ./tmp/parse/stable-m1-result.info
     sed -n '21p' ./tmp/parse/stable-x86.info >> ./tmp/parse/stable-m1-result.info
@@ -18,11 +18,11 @@ for i in $@; do
     echo 'dl.google.com/chrome/mac/universal/stable/GGRO/' >> ./tmp/parse/stable-m1-result.info
     echo 'googlechrome.dmg' >> ./tmp/parse/stable-m1-result.info
     
-    sed -n '14p' ./tmp/parse/beta-x64.info > ./tmp/parse/beta-inter-result.info
-    sed -n '21p' ./tmp/parse/beta-x64.info >> ./tmp/parse/beta-inter-result.info
-    sed -n '23p' ./tmp/parse/beta-x64.info >> ./tmp/parse/beta-inter-result.info
-    echo 'dl.google.com/chrome/mac/beta/' >> ./tmp/parse/beta-inter-result.info
-    echo 'googlechromebeta.dmg' >> ./tmp/parse/beta-inter-result.info
+    sed -n '14p' ./tmp/parse/beta-x64.info > ./tmp/parse/beta-intel-result.info
+    sed -n '21p' ./tmp/parse/beta-x64.info >> ./tmp/parse/beta-intel-result.info
+    sed -n '23p' ./tmp/parse/beta-x64.info >> ./tmp/parse/beta-intel-result.info
+    echo 'dl.google.com/chrome/mac/beta/' >> ./tmp/parse/beta-intel-result.info
+    echo 'googlechromebeta.dmg' >> ./tmp/parse/beta-intel-result.info
     
     sed -n '14p' ./tmp/parse/beta-x86.info > ./tmp/parse/beta-m1-result.info
     sed -n '21p' ./tmp/parse/beta-x86.info >> ./tmp/parse/beta-m1-result.info
@@ -30,11 +30,11 @@ for i in $@; do
     echo 'dl.google.com/chrome/mac/universal/beta/' >> ./tmp/parse/beta-m1-result.info
     echo 'googlechromebeta.dmg' >> ./tmp/parse/beta-m1-result.info
     
-    sed -n '14p' ./tmp/parse/dev-x64.info > ./tmp/parse/dev-inter-result.info
-    sed -n '21p' ./tmp/parse/dev-x64.info >> ./tmp/parse/dev-inter-result.info
-    sed -n '23p' ./tmp/parse/dev-x64.info >> ./tmp/parse/dev-inter-result.info
-    echo 'dl.google.com/chrome/mac/dev/' >> ./tmp/parse/dev-inter-result.info
-    echo 'googlechromedev.dmg' >> ./tmp/parse/dev-inter-result.info
+    sed -n '14p' ./tmp/parse/dev-x64.info > ./tmp/parse/dev-intel-result.info
+    sed -n '21p' ./tmp/parse/dev-x64.info >> ./tmp/parse/dev-intel-result.info
+    sed -n '23p' ./tmp/parse/dev-x64.info >> ./tmp/parse/dev-intel-result.info
+    echo 'dl.google.com/chrome/mac/dev/' >> ./tmp/parse/dev-intel-result.info
+    echo 'googlechromedev.dmg' >> ./tmp/parse/dev-intel-result.info
     
     sed -n '14p' ./tmp/parse/dev-x86.info > ./tmp/parse/dev-m1-result.info
     sed -n '21p' ./tmp/parse/dev-x86.info >> ./tmp/parse/dev-m1-result.info
@@ -42,11 +42,11 @@ for i in $@; do
     echo 'dl.google.com/chrome/mac/universal/dev/' >> ./tmp/parse/dev-m1-result.info
     echo 'googlechromedev.dmg' >> ./tmp/parse/dev-m1-result.info
     
-    sed -n '14p' ./tmp/parse/canary-x64.info > ./tmp/parse/canary-inter-result.info
-    sed -n '21p' ./tmp/parse/canary-x64.info >> ./tmp/parse/canary-inter-result.info
-    sed -n '23p' ./tmp/parse/canary-x64.info >> ./tmp/parse/canary-inter-result.info
-    echo 'dl.google.com/chrome/mac/canary/' >> ./tmp/parse/canary-inter-result.info
-    echo 'googlechromecanary.dmg' >> ./tmp/parse/canary-inter-result.info
+    sed -n '14p' ./tmp/parse/canary-x64.info > ./tmp/parse/canary-intel-result.info
+    sed -n '21p' ./tmp/parse/canary-x64.info >> ./tmp/parse/canary-intel-result.info
+    sed -n '23p' ./tmp/parse/canary-x64.info >> ./tmp/parse/canary-intel-result.info
+    echo 'dl.google.com/chrome/mac/canary/' >> ./tmp/parse/canary-intel-result.info
+    echo 'googlechromecanary.dmg' >> ./tmp/parse/canary-intel-result.info
     
     sed -n '14p' ./tmp/parse/canary-x86.info > ./tmp/parse/canary-m1-result.info
     sed -n '21p' ./tmp/parse/canary-x86.info >> ./tmp/parse/canary-m1-result.info
